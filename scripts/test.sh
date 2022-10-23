422ba12cb92ab2d5fe1c7e6f5027d6ab2bbb516e
Paolo Velati <paolo.velati@gmail.com>
2022-10-23 16:49:18 +0000

    test script variables

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
-# ------------- DEFINE VARIABLES ----------------
-# echo $BUILD_ARCH | tr 'a-z' 'A-Z' > build_arch_upper_tmp
-# export BAUP=$(cat build_arch_upper_tmp)
-# export BUILD_NUMBER=$(curl -s http://download.proxmox.com/debian/pve/dists/bullseye/pve-no-subscription/binary-amd64/Packages | grep ^Filename  | grep pve-kernel-5 | grep amd64.deb$ | sort -V | grep -oP 'kernel-5.15.\d+-\d+' | tail -1 | grep -o .$)
-# export PACKAGE_NUMBER=$(curl -s http://download.proxmox.com/debian/pve/dists/bullseye/pve-no-subscription/binary-amd64/Packages | grep ^Filename  | grep pve-kernel-5 | grep amd64.deb$ | sort -V | grep -oP 'pve_5.15.\d+-\d+' | tail -1 | grep -o .$)
+set -aux
+
+# ------------- DEFINE BUILD VARIABLES ----------------
+echo $BUILD_ARCH | tr 'a-z' 'A-Z' > build_arch_upper_tmp
+export "BAUP=$(cat build_arch_upper_tmp)"
+export BUILD_NUMBER=$(curl -s http://download.proxmox.com/debian/pve/dists/bullseye/pve-no-subscription/binary-amd64/Packages | grep ^Filename  | grep pve-kernel-5 | grep amd64.deb$ | sort -V | grep -oP 'kernel-5.15.\d+-\d+' | tail -1 | grep -o .$)
+export PACKAGE_NUMBER=$(curl -s http://download.proxmox.com/debian/pve/dists/bullseye/pve-no-subscription/binary-amd64/Packages | grep ^Filename  | grep pve-kernel-5 | grep amd64.deb$ | sort -V | grep -oP 'pve_5.15.\d+-\d+' | tail -1 | grep -o .$)
+
 
 echo "PRINT VARIABLES"
 echo "BUILD_ARCH: $BUILD_ARCH"
+echo "BAUP: $BAUP"
+echo "BUILD_NUMBER: $BUILD_NUMBER"
+# echo "PACKAGE_NUMBER: $PACKAGE_NUMBER"
 
-echo "KERNEL_VERSION=5.15" >> $GITHUB_ENV
+# ------------- DEFINE METAPACKAGE VARIABLES ----------------
+echo "CPU_ARCH=amd64" >> $GITHUB_ENV
+echo "KERNEL_VERSION=5.15.64-1" >> $GITHUB_ENV
 echo "META_VERSION=$(date -u +%y%m%d%H)" >> $GITHUB_ENV
+
+
+echo "PRINT VARIABLES"
+echo "CPU_ARCH $CPU_ARCH"
+echo "KERNEL_VERSION $KERNEL_VERSION"
+echo "META_VERSION $META_VERSION"
